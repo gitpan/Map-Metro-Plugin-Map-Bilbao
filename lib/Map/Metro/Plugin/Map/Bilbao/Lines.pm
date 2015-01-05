023c1e571f6ebef7e252fe985643af1dfeb3baf9
Erik Carlsson <info@code301.com>
2015-01-05 20:22:31 +0000

    Import of CSSON/Map-Metro-Plugin-Map-Bilbao-0.1000.tar.gz from CPAN.
    gitpan-cpan-distribution: Map-Metro-Plugin-Map-Bilbao
    gitpan-cpan-version:      0.1000
    gitpan-cpan-path:         CSSON/Map-Metro-Plugin-Map-Bilbao-0.1000.tar.gz
    gitpan-cpan-author:       CSSON
    gitpan-cpan-maturity:     released

---
+++ lib/Map/Metro/Plugin/Map/Bilbao/Lines.pm
@@ -0,0 +1,87 @@
+package Map::Metro::Plugin::Map::Bilbao::Lines;
+
+our $VERSION = '0.1101'; our $VERSION = '0.1000'; # VERSION
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Map::Metro::Plugin::Map::Bilbao::Lines - Detailed information about Map::Metro::Plugin::Map::Bilbao
+
+=head1 LINES
+
+=head2 Line 1: Etxebarri → Plentzia [L1]
+
+    Etxebarri (L2)
+    Bolueta (L2)
+    Basarrate (L2)
+    Santutxu (L2)
+    Casco Viejo (L2)
+    Abando (L2)
+    Moyua (L2)
+    Indautxu (L2)
+    San Mames (L2)
+    Deusto (L2)
+    Sarriko (L2)
+    San Inazio (L2)
+    Lutxana
+    Erandio
+    Astrabudua
+    Leioa
+    Lamiako
+    Areeta
+    Gobela
+    Neguri
+    Aiboa
+    Algorta
+    Bidezabal
+    Berango
+    Larrabasterra
+    Sopelana
+    Urduliz
+    Plentzia
+
+=head2 Line 2: Basauri → Kabiezes [L2]
+
+    Basauri
+    Ariz
+    Etxebarri (L1)
+    Bolueta (L1)
+    Basarrate (L1)
+    Santutxu (L1)
+    Casco Viejo (L1)
+    Abando (L1)
+    Moyua (L1)
+    Indautxu (L1)
+    San Mames (L1)
+    Deusto (L1)
+    Sarriko (L1)
+    San Inazio (L1)
+    Gurutzeta
+    Ansio
+    Barakaldo
+    Bagatza
+    Urbinaga
+    Sestao
+    Abatxolo
+    Portugalete
+    Peñota
+    Santurtzi (M)
+    Kabiezes
+
+=head2 Mamariga shuttle: Mamariga → Santurtzi [M]
+
+    Mamariga
+    Santurtzi (L2)
+
+
+=head1 SEE ALSO
+
+L<Map::Metro::Plugin::Map::Bilbao>
+
+=cut
+
